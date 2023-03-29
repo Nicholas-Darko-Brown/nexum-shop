1654b9de7a14e131827b2f6eb51433d72603f206
Nicholas-Darko-Brown <nicholas.brown@amalitech.org>
2023-03-29 11:50:03 +0000

    feat: add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "color-main-100": "#E8755B"
+      }
+    },
+  },
+  plugins: [],
+};
