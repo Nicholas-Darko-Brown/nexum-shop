8f309764b6e1ada2cff324d35fe5285fe62eaf9c
Nicholas-Darko-Brown <nicholas.brown@amalitech.org>
2023-03-29 11:49:49 +0000

    feat: project setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+import Layout from "@/components/Layout/Layout";
+
+const inter = Inter({
+  weight: ["200", "400", "700", "900"],
+  subsets: ["latin"],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
